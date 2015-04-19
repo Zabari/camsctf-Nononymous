0252733653e1d9ff0184583e2da6f9fc7ce93736
Zabari <Zabari.daniel@Gmail.com>
2015-04-19 16:27:11 -0400

    nothing

--- My-Caffeine_NOFLAG/post.py
+++ My-Caffeine_NOFLAG/post.py
@@ -3,21 +3,32 @@ headers={"Content-Type":"application/coffee-pot-command", "cookie":"PHPSESSID=25
 s = Session()
 url="http://coffee.camsctf.com/"
 #print s.get(url).text
-req = Request('POST',  url,
+req = Request('BREW',  url,
     headers=headers,
     data="23"
 )
 prepped = s.prepare_request(req)
 res=s.send(prepped)
+print res.text
 req=Request('WHEN',  url,
     headers=headers)
 prepped = s.prepare_request(req)
 res=s.send(prepped)
+print res.text
 req = Request('GET',  url,
     headers=headers
 )
-
-
 prepped = s.prepare_request(req)
 res=s.send(prepped)
 print res.text
+
+headers={"Content-Type":"+c70Xz/73+4riCRCllkBKm5MClqoTv7CUOJYtrS5LL8=", "cookie":"PHPSESSID=2537527762","Content-Type":"application/coffee-pot-command"}
+
+req = Request('GET',  url,
+    headers=headers,
+	data="23"
+)
+prepped = s.prepare_request(req)
+res=s.send(prepped)
+print res.text
+
