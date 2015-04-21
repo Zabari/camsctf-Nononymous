b39fa1f93f38ce0d4d1c88954b1476f4a4d1e39e
Zabari <Zabari.daniel@Gmail.com>
2015-04-20 21:19:08 -0400

    finallly

--- Impossible-Salt_NOFLAG/impossible_salt.py
+++ Impossible-Salt_NOFLAG/impossible_salt.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import hashlib, random
+import requests as re
 
 def salt_filename(filename):
 	return hashlib.sha512((generate_salt() + filename).encode()).hexdigest()[:50]
@@ -11,6 +12,9 @@ def generate_salt():
 	for i in range(0, 10000):
 		SALT=SALT+ str(random.randint(0, 9))
 	return hashlib.sha512(SALT.encode()).hexdigest()
-
-
-print salt_filename("flag.txt")
+url="http://cdn.camsctf.com/"
+for x in range(100):
+	thang=salt_filename("flag.txt")
+	r=re.get(url+thang)
+	if "{" in r.text:
+		print r.text
